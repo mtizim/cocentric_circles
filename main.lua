acdfb1b5a6a0a2aed57f247e89d28d2040987b4c
Mateusz Ziemła <mtizim1@gmail.com>
2019-03-02 22:00:11 +0100

    two_circles

--- main.lua
+++ main.lua
@@ -3,16 +3,17 @@ Class = require("dependencies.30log")
 circleClass = Class("circle")
 
 -- window width, height
+love.window.setMode(0,0,{fullscreen = true})
 ww, wh = love.graphics.getDimensions()
 -- biggest radius
 rstart = wh/2
 -- starting angular velocity
 ostart = 1
 -- every next circle radius multiplier
-rscale = 0.9
+rscale = 0.5
 -- every next circle angvel multiplier
 oscale = 1
-circle_count = 170
+circle_count = 5
 
 
 function circleClass:init(xrel,yrel,r,omega,childnum,parent)
@@ -23,6 +24,7 @@ function circleClass:init(xrel,yrel,r,omega,childnum,parent)
     self.rotation = 0
     if childnum > 0 then
         self.kid = circleClass(0,r*(1-rscale),r*rscale,omega*oscale,childnum-1,self)
+        self.kid2 = circleClass(0,-r*(1-rscale),r*rscale,omega*oscale,childnum-1,self)
     end
 end
 
@@ -30,6 +32,7 @@ function circleClass:update(dt)
     self.rotation = self.rotation + self.omega * dt
     if self.kid then
         self.kid:update(dt)
+        self.kid2:update(dt)
     end
 end
 
@@ -40,7 +43,12 @@ function circleClass:draw()
     -- drawing
     love.graphics.circle("line",0,0,self.r)
     if self.kid then
+        love.graphics.push()
         self.kid:draw()
+        love.graphics.pop()
+        love.graphics.push()
+        self.kid2:draw()
+        love.graphics.pop()
     end
 end
 
